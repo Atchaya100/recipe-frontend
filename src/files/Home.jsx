13d5585ab16d38408cb93a04de00c26a698bf2f6
Atchaya100 <atchaya00@gmail.com>
2023-11-20 12:53:21 +0530

    second

--- src/files/Home.jsx
+++ src/files/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../styles/Home.css';
 import { Link} from "react-router-dom";
-import front from '../assets/front.jpg';
+import front from '../assets/front.avif';
+import food from '../assets/food.avif';
 import { Button } from 'react-bootstrap';
 import { useEffect } from 'react';
 export default function Home() {
@@ -13,10 +14,14 @@ export default function Home() {
   }
 },[]);
   return (
-    <div id="center">
-      <img src={front}/><br></br>
+    <>
+    <div id="corner">
+    <img src={food}/><br></br>
+       <span>
       <h4>Create, Find New Recipes Everyday</h4><br></br>
       <Button id="h" variant="none"><Link to='/login'>Login</Link></Button>
+      </span>
     </div>
+    </>
   )
 }
